4e3a158c84f5dfc481a54a102fe74bb740540456
demikiwi <constandi.nicolas@gmail.com>
2021-09-24 15:13:47 +0200

    affichage liste des device

--- hello.js
+++ hello.js
@@ -1,14 +1,16 @@
+const http =  require('http')
 const express = require('express')
 const app = express()
 const port = 8080
+json = require('./BDD.json');
 
 app.get('/', (req,res) => {
     res.send('Bienvenue - Sauzer / Constandi')
   })
   
 app.get('/device_list',(req,res) => {
-  res.writeHead(200,{"Content-Type":"application/json"});
-  return res.json('../BDD.json');
+  res.setHeader("Content-Type","application/json")
+  return res.json(json)
 })
   
   app.listen(port,() => {
